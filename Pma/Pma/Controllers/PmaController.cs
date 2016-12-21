9723608adbbf24bff676bc7220bcea204ac0bc20
Haim Rozenblum <haim@zerto.com>
2016-12-21 16:44:49 +0200

    [master] Add database
    Branch: [master]

--- Pma/Pma/Controllers/PmaController.cs
+++ Pma/Pma/Controllers/PmaController.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using PmaEntities;
-using Ppa.Models;
 using Ppa.Services;
 
 namespace Ppa.Controllers
 {
     public class PmaController : ApiController
     {
-        //private ContactRepository contactRepository;
-        private PmaRepository m_pmaRepository;
+        private readonly PmaRepository m_pmaRepository;
 
         public PmaController()
         {
-            //this.contactRepository = new ContactRepository();
             m_pmaRepository = new PmaRepository();
         }
 
@@ -25,36 +17,5 @@ namespace Ppa.Controllers
         {
             return m_pmaRepository.GetAll();
         }
-
-        /*public Contact[] Get()
-        {
-            return contactRepository.GetAllContacts();
-        }*/
-
-        /*public Contact[] Get()
-        {
-            return new Contact[]
-            {
-                new Contact
-                {
-                    Id = 1,
-                    Name = "Glenn Block"
-                },
-                new Contact
-                {
-                Id = 2,
-                Name = "Dan Roth"
-            }
-            };
-        }*/
-
-        /*public string[] Get()
-        {
-            return new string[]
-            {
-             "Hello",
-             "World"
-            };
-        }*/
     }
 }
--- Pma/Pma/Services/PmaRepository.cs
+++ Pma/Pma/Services/PmaRepository.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
 using PmaEntities;
-using Ppa.Models;
 
 namespace Ppa.Services
 {
     public class PmaRepository
     {
+
+        private const string ConnectionString =@"Server=zdb\SQLSERVER2012;Database=pma;User Id=pma;Password=zertodata;";
+
         public PmaRawEntity[] GetAll()
         {
             return GenerateDummyPmaData(1000);
@@ -16,10 +21,46 @@ namespace Ppa.Services
             return GenerateDummyPmaData(1000);
         }
 
-//        public PmaData[] GetFilteredData(DateTime from, DateTime to)
-//        {
-//            return GenerateDummyPmaData(1000);
-//        }
+        public void SetData(List<PmaRawEntity> pmaList)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+
+                using (var bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, transaction))
+                {
+                    bulkCopy.BatchSize = 100;
+                    bulkCopy.DestinationTableName = "dbo.PmaRawData";
+                    try
+                    {
+                        bulkCopy.WriteToServer(ConvertListToDataTable(pmaList));
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        connection.Close();
+                    }
+                }
+                transaction.Commit();
+            }
+        }
+
+        public static DataTable ConvertListToDataTable<T>(List<T> data)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
 
 
         private PmaRawEntity[] GenerateDummyPmaData(int numberOfRows)
@@ -37,21 +78,23 @@ namespace Ppa.Services
         private PmaRawEntity CreatePmaRow(DateTime dateTime, int index)
         {
             Random random = new Random(index);
-            
-            PmaRawEntity pmaRawEntity = new PmaRawEntity();
-            pmaRawEntity.TimeStamp = dateTime;
-            pmaRawEntity.ProtectedVolumeWriteRateMbs = Math.Abs(random.NextDouble());
-            pmaRawEntity.ProtectedVolumeCompressedWriteRateMBs = Math.Abs(random.NextDouble());
-            pmaRawEntity.ProtectedCpuPerc = random.Next(0, 100);
-            pmaRawEntity.ProtectedVraBufferUsagePerc = random.Next(0, 100);
-            pmaRawEntity.ProtectedTcpBufferUsagePerc = random.Next(0, 100);
-            pmaRawEntity.NetworkOutgoingRateMBs = Math.Abs(random.NextDouble());
-            pmaRawEntity.RecoveryTcpBufferUsagePerc = random.Next(0, 100);
-            pmaRawEntity.RecoveryCpuPerc = random.Next(0, 100);
-            pmaRawEntity.RecoveryVraBufferUsagePerc = random.Next(0, 100);
-            pmaRawEntity.HardeningRateMBs = Math.Abs(random.NextDouble());
-            pmaRawEntity.JournalSizeMB = Math.Abs(random.NextDouble());
-            pmaRawEntity.ApplyRateMBs = Math.Abs(random.NextDouble());
+
+            PmaRawEntity pmaRawEntity = new PmaRawEntity
+            {
+                TimeStamp = dateTime,
+                ProtectedVolumeWriteRateMbs = Math.Abs(random.NextDouble()),
+                ProtectedVolumeCompressedWriteRateMBs = Math.Abs(random.NextDouble()),
+                ProtectedCpuPerc = random.Next(0, 100),
+                ProtectedVraBufferUsagePerc = random.Next(0, 100),
+                ProtectedTcpBufferUsagePerc = random.Next(0, 100),
+                NetworkOutgoingRateMBs = Math.Abs(random.NextDouble()),
+                RecoveryTcpBufferUsagePerc = random.Next(0, 100),
+                RecoveryCpuPerc = random.Next(0, 100),
+                RecoveryVraBufferUsagePerc = random.Next(0, 100),
+                HardeningRateMBs = Math.Abs(random.NextDouble()),
+                JournalSizeMB = Math.Abs(random.NextDouble()),
+                ApplyRateMBs = Math.Abs(random.NextDouble())
+            };
 
             //            {
             //                DateTime = dateTime,
