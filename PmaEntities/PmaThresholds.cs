9669e3ef0a3653d62ec818a3830fd7c6e84a714a
Haim Rozenblum <haim@zerto.com>
2016-12-21 19:14:12 +0200

    [master] Add Threshold
    Branch: [master]

---
+++ PmaEntities/PmaThresholds.cs
@@ -0,0 +1,12 @@
+﻿namespace PmaEntities
+{
+    public class PmaThresholds
+    {
+        public int ProtectedCpuPerc { get { return 80; }}
+        public int ProtectedVraBufferUsagePerc { get { return 80; } }
+        public int ProtectedTcpBufferUsagePerc { get { return 80; } }
+        public int RecoveryTcpBufferUsagePerc { get { return 80; } }
+        public int RecoveryCpuPerc { get { return 80; } }
+        public int RecoveryVraBufferUsagePerc { get { return 80; } }
+    }
+}
