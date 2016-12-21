44ce3a220c7762468edbeadd95a71c18eafc8c5a
Haim Rozenblum <haim@zerto.com>
2016-12-21 23:25:08 +0200

    [master] Fix on Fix on Fix
    Branch: [master]

--- PmaEntities/PmaRawFieldData.cs
+++ PmaEntities/PmaRawFieldData.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace PmaEntities
 {
+    [DataContract]
     public class PmaRawFieldData
     {
+        [DataMember]
         public string FieldName { get; set; }
+        [DataMember]
         public string Value { get; set; }
+        [DataMember]
         public string Threshold { get; set; }
+        [DataMember]
         public int IsValid { get; set; }
 
         public PmaRawFieldData(string fieldName, string value, string threshold, int isValid)
