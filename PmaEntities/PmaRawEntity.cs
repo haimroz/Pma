3bc4c86afc342cf668f31889739218eaea590811
Yaniv Menachem <yaniv.menachem@zerto.com>
2017-02-17 00:29:14 +0200

    [master] Improve ToString
    Branch: [master]

--- PmaEntities/PmaRawEntity.cs
+++ PmaEntities/PmaRawEntity.cs
@@ -123,7 +123,7 @@ namespace PmaEntities
         public override string ToString()
         {
             return string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}", 
-                TimeStamp.ToShortDateString(),
+                TimeStamp,
                 ProtectedIOsInDriverMBs,
                 ProtectedVolumeWriteRateMBs,
                 ProtectedVolumeCompressedWriteRateMBs,
